06296108d8e46dc9af5d2505685a592fec49d648
androidProject <alimasudd.dev@gmail.com>
2020-08-27 14:37:05 +0700

    flutter-covid-19

---
+++ lib/data/data.dart
@@ -0,0 +1,5 @@
+final prevention = [
+  {'assets/images/distance.png': 'Jaga jarak'},
+  {'assets/images/wash_hands.png': 'Sering cuci tangan'},
+  {'assets/images/mask.png': 'Pakai masker'},
+];
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,299 @@
+import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_covid19/config/network.dart';
+import 'package:flutter_covid19/config/palette.dart';
+import 'package:flutter_covid19/config/styles.dart';
+import 'package:flutter_covid19/data/data.dart';
+import 'package:flutter_covid19/widgets/custom_app_bar.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future data;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    data = getDataIndo();
+    data.then((value) => {
+      print(value[0]["name"])
+    });
+  }
+
+  Future getDataIndo() async {
+    Network network = Network(url: "https://api.kawalcorona.com/indonesia/");
+    return network.fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: CustomAppBar(),
+      body: CustomScrollView(
+        physics: ClampingScrollPhysics(),
+        slivers: <Widget>[
+          _buildHeader(screenHeight),
+          _buildPreventionTips(screenHeight),
+          _buildRegionTabBar(),
+          _dataFromAPI(),
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            sliver: SliverToBoxAdapter(
+//              child: StatsGrid(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildHeader(double screenHeight) {
+    return SliverToBoxAdapter(
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          color: Palette.primaryColor,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(40.0),
+            bottomRight: Radius.circular(40.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'COVID-19',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+//                CountryDropdown(
+//                  countries: ['CN', 'FR', 'IN', 'IT', 'UK', 'USA'],
+//                  country: _country,
+//                  onChanged: (val) => setState(() => _country = val),
+//                ),
+              ],
+            ),
+            SizedBox(height: screenHeight * 0.03),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Apa anda merasa sakit?',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.01),
+                Text(
+                  'Jika Anda merasakan gejala COVID-19, silahkan hubungi atau gunakan panggilan darurat',
+                  style: const TextStyle(
+                    color: Colors.white70,
+                    fontSize: 15.0,
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.03),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    FlatButton.icon(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 20.0,
+                      ),
+                      onPressed: () {},
+                      color: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      icon: const Icon(
+                        Icons.phone,
+                        color: Colors.white,
+                      ),
+                      label: Text(
+                        'Panggilan Darurat',
+                        style: Styles.buttonTextStyle,
+                      ),
+                      textColor: Colors.white,
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildPreventionTips(double screenHeight) {
+    return SliverToBoxAdapter(
+      child: Container(
+        margin: EdgeInsets.only(bottom: 50, top: 50),
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Tips Pencegahan',
+              style: const TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: prevention
+                  .map((e) => Column(
+                        children: <Widget>[
+                          Image.asset(
+                            e.keys.first,
+                            height: screenHeight * 0.12,
+                          ),
+                          SizedBox(height: screenHeight * 0.015),
+                          Text(
+                            e.values.first,
+                            style: const TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w500,
+                            ),
+                            textAlign: TextAlign.center,
+                          )
+                        ],
+                      ))
+                  .toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildRegionTabBar() {
+    return SliverToBoxAdapter(
+      child: DefaultTabController(
+        length: 2,
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20.0),
+          height: 50.0,
+          decoration: BoxDecoration(
+            color: Colors.deepPurpleAccent,
+            borderRadius: BorderRadius.circular(25.0),
+          ),
+          child: TabBar(
+            indicator: BubbleTabIndicator(
+              tabBarIndicatorSize: TabBarIndicatorSize.tab,
+              indicatorHeight: 40.0,
+              indicatorColor: Colors.white,
+            ),
+            labelStyle: Styles.tabTextStyle,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.white,
+            tabs: <Widget>[
+              Text('Indonesia'),
+              Text('Global'),
+            ],
+            onTap: (index) {
+              switch (index) {
+                case 0:
+                  print("value");
+                  break;
+                case 1:
+                  print("indo");
+                  break;
+              }
+            },
+//              print("value");
+//              print("indo");},
+          ),
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _dataFromAPI(){
+    return SliverToBoxAdapter(
+      child: FutureBuilder(
+          future: data,
+          builder: (context, AsyncSnapshot snapshot){
+            if(snapshot.hasData){
+              return Container(
+                margin: EdgeInsets.only(top: 20, right: 10, left: 10),
+                height: MediaQuery.of(context).size.height * 0.25,
+                child: Column(
+                  children: <Widget>[
+                    Flexible(
+                      child: Row(
+                        children: <Widget>[
+                          _buildStatCard('Total Positif', '${snapshot.data[0]["positif"]}', Colors.orange),
+                          _buildStatCard('Meninggal', '${snapshot.data[0]["meninggal"]}', Colors.red),
+                        ],
+                      ),
+                    ),
+                    Flexible(
+                      child: Row(
+                        children: <Widget>[
+                          _buildStatCard('Sembuh', '${snapshot.data[0]["sembuh"]}', Colors.green),
+                          _buildStatCard('Dirawat', '${snapshot.data[0]["dirawat"]}', Colors.lightBlue),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }else{
+              return LinearProgressIndicator();
+            }
+          }),
+
+    );
+  }
+
+  Expanded _buildStatCard(String title, String count, MaterialColor color) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              title,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 15.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Text(
+              count,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
